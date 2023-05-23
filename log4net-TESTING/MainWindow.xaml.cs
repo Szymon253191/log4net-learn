a812c12ad9add111c2ff13a5dd0b9ab5d60f21d5
Szymon <62205129+Szymon253191@users.noreply.github.com>
2023-05-23 16:00:02 +0200

    Added filters for logger

--- log4net-TESTING/MainWindow.xaml.cs
+++ log4net-TESTING/MainWindow.xaml.cs
@@ -23,23 +23,25 @@ namespace log4net_TESTING
         private void Btn1_Click(object sender, RoutedEventArgs e)
         {
             log.Info("Button1 clicked");
-            Label1.Content = "Button1 clicked";
+            Label1.Content = "Info sent";
         }
 
         private void Btn2_Click(object sender, RoutedEventArgs e)
         {
-            log.Info("Button2 clicked");
-            Label1.Content = "Button2 clicked";
+            log.Warn("Button2 clicked");
+            Label1.Content = "Warn sent";
         }
 
         private void Btn3_Click(object sender, RoutedEventArgs e)
         {
-
+            log.Error("Button3 clicked");
+            Label1.Content = "Error sent";
         }
 
         private void Btn4_Click(object sender, RoutedEventArgs e)
         {
-
+            log.Fatal("Button4 clicked");
+            Label1.Content = "Fatal sent";
         }
     }
 }
