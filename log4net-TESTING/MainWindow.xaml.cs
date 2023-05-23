f0e2c5c8069037c360e20ded4026acec9990542c
Szymon <62205129+Szymon253191@users.noreply.github.com>
2023-05-23 15:07:16 +0200

    FirstPush
    Creation of log4net loggers in app.config: Console, File, RollingFile

---
+++ log4net-TESTING/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace log4net_TESTING
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly log4net.ILog log =
+            LogHelper.GetLogger();
+          //log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            log.Info("INITIALIZATION");
+            Label1.Content = log.ToString();
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            log.Info("Button1 clicked");
+            Label1.Content = "Button1 clicked";
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            log.Info("Button2 clicked");
+            Label1.Content = "Button2 clicked";
+        }
+
+        private void Btn3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
