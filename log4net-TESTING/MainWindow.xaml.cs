6043ace824ffccb6a38f91453243efe5075073f4
Szymon <62205129+Szymon253191@users.noreply.github.com>
2023-05-24 09:19:49 +0200

    Created starting class and logger outside WPF page

--- log4net-TESTING/MainWindow.xaml.cs
+++ log4net-TESTING/MainWindow.xaml.cs
@@ -8,39 +8,28 @@ namespace log4net_TESTING
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-        private static readonly log4net.ILog log =
-            LogHelper.GetLogger();
-          //log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            log.Info("INITIALIZATION");
-            Label1.Content = log.ToString();
-        }
-
+    {       
         private void Btn1_Click(object sender, RoutedEventArgs e)
         {
-            log.Info("Button1 clicked");
+            Program.log.Info("Button1 clicked");
             Label1.Content = "Info sent";
         }
 
         private void Btn2_Click(object sender, RoutedEventArgs e)
         {
-            log.Warn("Button2 clicked");
+            Program.log.Warn("Button2 clicked");
             Label1.Content = "Warn sent";
         }
 
         private void Btn3_Click(object sender, RoutedEventArgs e)
         {
-            log.Error("Button3 clicked");
+            Program.log.Error("Button3 clicked");
             Label1.Content = "Error sent";
         }
 
         private void Btn4_Click(object sender, RoutedEventArgs e)
         {
-            log.Fatal("Button4 clicked");
+            Program.log.Fatal("Button4 clicked");
             Label1.Content = "Fatal sent";
         }
     }
---
+++ log4net-TESTING/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using log4net;
+
+namespace log4net_TESTING
+{
+    public class Program
+    {
+        public static readonly log4net.ILog log =
+        LogHelper.GetLogger();
+        //log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
+    }
+}
